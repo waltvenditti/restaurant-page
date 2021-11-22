f40c18265a89c594d38df3c2ab011ea0598d3daf
WaltVenditi <waltvenditti@gmail.com>
2021-11-22 09:39:30 -0500

    added various webpack properties and HTML file now being generated by JS

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("console.log('Test successful.');\n\nconst header = document.createElement('h1');\nconst img = document.createElement('img');\nconst copy = document.createElement('p');\nconst contentDiv = document.querySelector('#content');\n\nheader.textContent = 'RESTAURANT';\nimg.src = './main-image.jpg';\nimg.style['height'] = '400px';\ncopy.textContent = \"Try the most legendary food in town.\"\n\ncontentDiv.appendChild(header);\ncontentDiv.textContent = 'test2';\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
